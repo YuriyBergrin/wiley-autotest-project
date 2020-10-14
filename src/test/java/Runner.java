4a6e7044738f06cb313dac8f8580538c6a5f259b
YuriyBergrin <bergrin1991@gmail.com>
2020-10-14 19:31:16 +0300

    edit image download

---
+++ src/test/java/Runner.java
@@ -0,0 +1,12 @@
+import apitests.ApiTestWiley;
+import apitests.HttpbinImageTest;
+import apitests.HttpbinTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import uitests.UiTest;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ApiTestWiley.class, HttpbinTest.class, HttpbinImageTest.class, UiTest.class} )
+public class Runner {
+
+}
